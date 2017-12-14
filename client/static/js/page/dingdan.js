63dd600c3ac3a06c28bfc9d44dab2b98d84d9923
Rengf <1032387871@qq.com>
2017-12-14 21:02:17 +0800

    更新

--- client/static/js/page/dingdan.js
+++ client/static/js/page/dingdan.js
@@ -12,22 +12,53 @@ $(function() {
         dataType: "json",
         data: { _userId: userId },
         success: function(messageInfo) {
-            var i;
-            var div = "<div class='cartGoods'>" +
-                "<span class='name'>" + messageInfo.orderList[i]._mallId + "</span>" +
-                "<span class='count'>" + messageInfo.orderList[i].inventory + "</span>" +
-                "<span class='consignee'>" + messageInfo.orderList[i].consignee + "</span>" +
-                "<span class='consigneePhone'>" + messageInfo.orderList[i].consigneePhone + "</span>" +
-                "<span class='consigneeAddress'>" + messageInfo.orderList[i].consigneeAddress + "</span>" +
-                "<span class='integration'>" + messageInfo.orderList[i].integration + "</span>" +
-                "<span class='isExamine'>" + messageInfo.orderList[i].isExamine + " </span>" +
-                "<span class='Deliver_goods'>" + messageInfo.orderList[i].Deliver_goods + "</span>" +
-                "</div>";
-            alert(messageInfo.orderList.length)
-            for (i = 0; i < messageInfo.orderList.length; i++) {
-                $("#cart").find('.cartList').append(div);
-            }
+            var length = messageInfo.orderList.length;
+            if (length == 0) {
+                $('#cart').find('.emptyCart').css("display", "block")
+                $('#cart').find('.goodsCart').css("display", "none")
+            } else {
+                $('#cart').find('.emptyCart').css("display", "none")
+                $('#cart').find('.goodsCart').css("display", "block")
+                for (var i = 0; i < length; i++) {
+                    var mallName = messageInfo.orderList[i].mallName;
+                    var inventory = messageInfo.orderList[i].inventory;
+                    var consignee = messageInfo.orderList[i].consignee;
+                    var consigneePhone = messageInfo.orderList[i].consigneePhone;
+                    var consigneeAddress = messageInfo.orderList[i].consigneeAddress;
+                    var integration = messageInfo.orderList[i].integration;
+                    var fail = messageInfo.orderList[i].fail;
+                    var isExamine = function() {
+                        if (messageInfo.orderList[i].isExamine == true) {
+                            if (fail == true) {
+                                return "订单失败";
+                            } else {
+                                return "审核通过";
+                            }
+                        } else {
+                            return "审核中";
+                        }
+                    };
+                    var Deliver_goods = function() {
+                        if (messageInfo.orderList[i].Deliver_goods == true) {
+                            return "已发货";
+                        } else {
+                            return "未发货"
+                        }
+                    };
+                    var div = "<div class='cartGoods'>" +
+                        "<span class='name'>" + mallName + "</span>" +
+                        "<span class='count'>" + inventory + "</span>" +
+                        "<span class='consignee'>" + consignee + "</span>" +
+                        "<span class='consigneePhone'>" + consigneePhone + "</span>" +
+                        "<span class='consigneeAddress'>" + consigneeAddress + "</span>" +
+                        "<span class='integration'>" + integration + "</span>" +
+                        "<span class='isExamine'>" + isExamine() + " </span>" +
+                        "<span class='Deliver_goods'>" + Deliver_goods() + "</span>" +
+                        "</div>";
+                    $("#cart").find('.cartList').append(div);
+                }
 
+            }
         }
     })
 })
